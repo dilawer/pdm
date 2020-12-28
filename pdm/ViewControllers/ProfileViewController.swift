b2b20dfd176ec98b27fdeaacc22ffc66c0d509a4
Muhammad Aqeel <muhammad.aqeel@tabsusa.com>
2020-12-28 11:17:16 +0500

    task_id: https://share.clickup.com/t/h/1veyx7/ULZXPL11YW7VW55 description: Profile Page Code Cleaning

--- pdm/ViewControllers/ProfileViewController.swift
+++ pdm/ViewControllers/ProfileViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 import Alamofire
 
-class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource, UITableViewDelegate, UITableViewDataSource {
+class ProfileViewController: UIViewController {
     
-
+    //MARK:- Outlets
     @IBOutlet weak var uploadPostsTapped: UIView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profileSettingButton: UIButton!
@@ -29,7 +29,6 @@ class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollect
     @IBOutlet weak var uploadedPods: UILabel!
     @IBOutlet weak var bottomConstant: NSLayoutConstraint!
     @IBOutlet weak var heightConstant: NSLayoutConstraint!
-    
     @IBOutlet weak var tfName: UITextField!
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfAge: UITextField!
@@ -38,90 +37,6 @@ class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollect
     @IBAction func actionLiked(_ sender: Any) {
         let vc = storyboard?.instantiateViewController(withIdentifier: "LikedViewController") as! LikedViewController
         self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    //MARK:- Veriables
-    var centerY:CGFloat = 0.0
-    var isShowing = false
-    var recentPlayedEpisodes: [Episode]=[]
-    var settingsHeightConstant:CGFloat = 0.0
-    var arrayHistory = [Pod]()
-    
-    let profiletitleArr = ["In the Mix","the friend Zone","Shots Film","Kind Advise","Good Advise"]
-    let profilesubtitleArr = ["Episode Name","Episode Name","Episode Name","Episode Name","Episode Name"]
-    let profiletimeArr = ["40.00","40.00","40.00","40.00","40.00"]
-    
-    let profileimageArr: [UIImage] = [
-        UIImage(named: "recomone")!,
-        UIImage(named: "releaseone")!,
-        UIImage(named: "recomtwo")!,
-        UIImage(named: "trendingthree")!,
-        UIImage(named: "trendingone")!,
-        UIImage(named: "trendingtwo")!,
-    ]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let itemSize = UIScreen.main.bounds.width/3 - 2
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: itemSize + 40, height: itemSize + 40)
-        layout.minimumInteritemSpacing = 2
-        layout.minimumLineSpacing = 15
-        profilecollectionview.collectionViewLayout = layout
-        settingsView.layer.masksToBounds = true
-        settingsView.roundCorners(corners: [.topLeft,.topRight], radius: 10)
-        settingsView.isHidden = true
-//        hideBottom()
-        self.uploadHistoryView.isHidden = true
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-        uploadPostsTapped.isUserInteractionEnabled = true
-        uploadPostsTapped.addGestureRecognizer(tapGestureRecognizer)
-        uploadHistoryView.layer.masksToBounds = true
-        uploadHistoryView.roundCorners(corners: [.topLeft,.topRight], radius: 10)
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped1(tapGestureRecognizer:)))
-        self.view.addGestureRecognizer(tap)
-        
-        let user = User.getInstance()
-        self.totalListens.text = user?.totalListens
-        self.totalMins.text = user?.totalMins
-        self.uploadedPods.text = user?.totalUploads
-        self.userNameLabel.text = user?.fullName
-        self.tfName.text = user?.fullName
-        self.tfName.text = user?.fullName
-        // Use Alamofire to download the image
-        ImageLoader.loadImage(imageView: profileDp, url: user?.profile_image ?? "")
-        ImageLoader.loadImage(imageView: profileImageView, url: user?.profile_image ?? "")
-        ImageLoader.loadImage(imageView: coverImageView, url: user?.cover_image ?? "")
-        WebManager.getInstance(delegate: self)?.getProfileDetail()
-        WebManager.getInstance(delegate: self)?.getPdmHistory()
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        settingsHeightConstant = self.view.frame.height / 2
-        let user = User.getInstance()
-        tfEmail.text = user?.email
-        tfAge.text = user?.dob
-        tfName.text = user?.fullName
-    }
-    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
-        self.userNameLabel.isHidden = true
-        self.profileSettingButton.isHidden = true
-        self.podView.alpha = 0
-        self.totalRecordView.alpha = 0
-        self.songsMainView.alpha = 0
-//        self.settingsView.isHidden = true
-        hideBottom()
-        self.uploadHistoryView.isHidden = false
-    }
-    
-    @objc func imageTapped1(tapGestureRecognizer: UITapGestureRecognizer) {
-        self.userNameLabel.isHidden = false
-        self.profileSettingButton.isHidden = false
-        self.podView.alpha = 1
-        self.totalRecordView.alpha = 1
-        self.songsMainView.alpha = 1
-//        self.settingsView.isHidden = true
-        hideBottom()
-        self.uploadHistoryView.isHidden = true
     }
     @IBAction func logoutAction(_ sender: Any) {
         let alert = UIAlertController(title: "Alert",
@@ -158,34 +73,108 @@ class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollect
             UIApplication.shared.open(url)
         }
     }
-    
     @IBAction func backBtnTapped(_ sender: Any) {
         self.userNameLabel.isHidden = false
         self.profileSettingButton.isHidden = false
         self.podView.alpha = 1
         self.totalRecordView.alpha = 1
         self.songsMainView.alpha = 1
-//        self.settingsView.isHidden = true
         hideBottom()
         self.uploadHistoryView.isHidden = true
     }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return recentPlayedEpisodes.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let vctwo = storyboard?.instantiateViewController(withIdentifier: "selectedPodcastViewController") as? selectedPodcastViewController;
-        self.navigationController?.pushViewController(vctwo!, animated: true)
-    }
-    
     @IBAction func settingsBtnTapped(_ sender: Any) {
         if self.settingsView.isHidden{
             showBottom()
         } else {
             hideBottom()
         }
+    }
+    
+    //MARK:- Veriables
+    var centerY:CGFloat = 0.0
+    var isShowing = false
+    var recentPlayedEpisodes: [Episode]=[]
+    var settingsHeightConstant:CGFloat = 0.0
+    var arrayHistory = [Pod]()
+    
+    let profiletitleArr = ["In the Mix","the friend Zone","Shots Film","Kind Advise","Good Advise"]
+    let profilesubtitleArr = ["Episode Name","Episode Name","Episode Name","Episode Name","Episode Name"]
+    let profiletimeArr = ["40.00","40.00","40.00","40.00","40.00"]
+    
+    let profileimageArr: [UIImage] = [
+        UIImage(named: "recomone")!,
+        UIImage(named: "releaseone")!,
+        UIImage(named: "recomtwo")!,
+        UIImage(named: "trendingthree")!,
+        UIImage(named: "trendingone")!,
+        UIImage(named: "trendingtwo")!,
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        config()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        settingsHeightConstant = self.view.frame.height / 2
+        let user = User.getInstance()
+        tfEmail.text = user?.email
+        tfAge.text = user?.dob
+        tfName.text = user?.fullName
+    }
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        self.userNameLabel.isHidden = true
+        self.profileSettingButton.isHidden = true
+        self.podView.alpha = 0
+        self.totalRecordView.alpha = 0
+        self.songsMainView.alpha = 0
+        hideBottom()
+        self.uploadHistoryView.isHidden = false
+    }
+    
+    @objc func imageTapped1(tapGestureRecognizer: UITapGestureRecognizer) {
+        self.userNameLabel.isHidden = false
+        self.profileSettingButton.isHidden = false
+        self.podView.alpha = 1
+        self.totalRecordView.alpha = 1
+        self.songsMainView.alpha = 1
+        hideBottom()
+        self.uploadHistoryView.isHidden = true
+    }
+}
+//MARK:- Methods
+extension ProfileViewController{
+    func config(){
+        let itemSize = UIScreen.main.bounds.width/3 - 2
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: itemSize + 40, height: itemSize + 40)
+        layout.minimumInteritemSpacing = 2
+        layout.minimumLineSpacing = 15
+        profilecollectionview.collectionViewLayout = layout
+        settingsView.layer.masksToBounds = true
+        settingsView.roundCorners(corners: [.topLeft,.topRight], radius: 10)
+        settingsView.isHidden = true
+        self.uploadHistoryView.isHidden = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        uploadPostsTapped.isUserInteractionEnabled = true
+        uploadPostsTapped.addGestureRecognizer(tapGestureRecognizer)
+        uploadHistoryView.layer.masksToBounds = true
+        uploadHistoryView.roundCorners(corners: [.topLeft,.topRight], radius: 10)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped1(tapGestureRecognizer:)))
+        self.view.addGestureRecognizer(tap)
         
- 
+        let user = User.getInstance()
+        self.totalListens.text = user?.totalListens
+        self.totalMins.text = user?.totalMins
+        self.uploadedPods.text = user?.totalUploads
+        self.userNameLabel.text = user?.fullName
+        self.tfName.text = user?.fullName
+        self.tfName.text = user?.fullName
+        
+        ImageLoader.loadImage(imageView: profileDp, url: user?.profile_image ?? "")
+        ImageLoader.loadImage(imageView: profileImageView, url: user?.profile_image ?? "")
+        ImageLoader.loadImage(imageView: coverImageView, url: user?.cover_image ?? "")
+        WebManager.getInstance(delegate: self)?.getProfileDetail()
+        WebManager.getInstance(delegate: self)?.getPdmHistory()
     }
     func showBottom(){
         self.settingsView.isHidden = false
@@ -204,6 +193,17 @@ class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollect
             self.settingsView.isHidden = true
         })
     }
+}
+
+//MARK:- CollectionView
+extension ProfileViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recentPlayedEpisodes.count
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vctwo = storyboard?.instantiateViewController(withIdentifier: "selectedPodcastViewController") as? selectedPodcastViewController;
+        self.navigationController?.pushViewController(vctwo!, animated: true)
+    }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "profilecell", for: indexPath) as! ProfileCollectionViewCell
         let cellIndex = indexPath.item
@@ -212,10 +212,12 @@ class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollect
         cell.celltime.text = recentPlayedEpisodes[cellIndex].duration
         WebManager.getInstance(delegate: self)?.downloadImage(imageUrl: recentPlayedEpisodes[cellIndex].icon, imageView: cell.profileimage)
         cell.layer.cornerRadius = 10
-        
         return cell
     }
-    
+}
+
+//MARK:- TableView
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
@@ -251,6 +253,7 @@ class ProfileViewController: UIViewController,UICollectionViewDelegate,UICollect
     }
 }
 
+//MARK:- API'S
 extension ProfileViewController: WebManagerDelegate {
     func failureResponse(response: AFDataResponse<Any>) {
         Utility.showAlertWithSingleOption(controller: self, title: kEmpty, message: kCannotConnect, preferredStyle: .alert, buttonText: kok, buttonHandler: nil)
